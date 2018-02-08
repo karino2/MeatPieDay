716dc63a3dd11c10da61dc3f28dee20e65339a38
karino2 <hogeika2@gmailcom>
2018-02-08 17:19:49 +0900

    Add link generation from clipboard menu.

--- app/src/main/java/karino2/livejournal/com/meatpieday/EditActivity.java
+++ app/src/main/java/karino2/livejournal/com/meatpieday/EditActivity.java
@@ -1,10 +1,14 @@
 package karino2.livejournal.com.meatpieday;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,6 +53,53 @@ public class EditActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        et.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                menu.add(Menu.NONE, R.id.action_link, Menu.NONE, "Link");
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return true;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.action_link) {
+                    int start = et.getSelectionStart();
+                    int end = et.getSelectionEnd();
+                    int min = Math.min(start, end);
+                    int max = Math.max(start, end);
+
+                    CharSequence link = "";
+
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
+
+                    link = item.getText();
+                    if(link == null) {
+                        link = "";
+                    }
+
+                    CharSequence text = et.getText();
+
+                    CharSequence target = text.subSequence(min, max);
+                    et.setText(text.subSequence(0, min) + "[" + target + "](" + link + ")"  + text.subSequence(max, text.length()));
+
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        });
+
 
         Intent intent = getIntent();
         if(intent != null) {
